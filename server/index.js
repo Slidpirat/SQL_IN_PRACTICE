639c4eea245b49157d03aa2b21bcd6015d04e207
chrisdickerman93@gmail.com <74125686+Slidpirat@users.noreply.github.com>
2021-12-14 18:49:30 -0700

    first Commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { SERVER_PORT } = process.env;
+const {
+  getAllClients,
+  getPendingAppointments,
+  getUpcomingAppointments,
+  getPastAppointments,
+  approveAppointment,
+  completeAppointment,
+  deleteAppointment,
+} = require("./controller.js");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/clients", getAllClients);
+
+app.get("/pending", getPendingAppointments);
+app.get("/upcoming", getUpcomingAppointments);
+app.get("/appt", getPastAppointments);
+app.put("/approve", approveAppointment);
+app.put("/complete", completeAppointment);
+app.delete("/appt", deleteAppointment);
+
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`));
